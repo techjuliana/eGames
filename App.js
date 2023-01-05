cb239462a54fe386635f990a49ac6e3bccaed985
JulianaGok <juliana.bitencourt@gok.digital>
2023-01-05 13:13:08 -0300

    finalizado

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Routes from "./src/routes";
-import { tema } from "./src/theme/theme";
 import { NavigationContainer } from "@react-navigation/native";
-
 export default function App() {
   return (
-    <NavigationContainer theme={tema}>
+    <NavigationContainer>
       <Routes />
     </NavigationContainer>
   );
--- src/telas/cadastro/styled.js
+++ src/telas/cadastro/styled.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#101114",
+    backgroundColor: "#101114", 
     height:'100%',
   },
   containerEstudante: {
